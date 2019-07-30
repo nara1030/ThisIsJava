a2f6bd8ea302dd163a286d6fc543408e5f58dcfd
nara1030 <nara1030@naver.com>
2019-07-30 16:26:02 +0900

    스트림과 병렬처리

---
+++ thisIsJava_ch16/src/ex_04_stream_filtering/FilteringExample.java
@@ -0,0 +1,10 @@
+package ex_04_stream_filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FilteringExample {
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("홍길동", "신용권", "김자바", "신용권", "신민철");
+	}
+}
